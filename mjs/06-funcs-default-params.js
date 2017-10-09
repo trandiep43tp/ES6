fd417a4e7097ea6cf9502e701862b8f44752328b
Tran Diep <trandiep43tp.com>
2017-10-09 15:27:20 +0700

    add file 06.js

---
+++ mjs/06-funcs-default-params.js
@@ -0,0 +1,13 @@
+
+
+
+//đặt giá trị mặc định vào biến khi viết hàm ====>func-default-params. nó giúp tránh lỗi undefine
+function show(name="diep",price="26"){
+	return `<div>Tên khóa học: <b>${name}</b> Giá khóa học: <b>${price}</b></div>`;
+}
+
+$(document).ready(function(){
+	$("#mContent").css("color","#0174DF");
+	//$("#mContent").html(show());  //khi không truyền giá trị
+	$("#mContent").html(show("hùng", 30));  //khi truyền giá trị
+});
